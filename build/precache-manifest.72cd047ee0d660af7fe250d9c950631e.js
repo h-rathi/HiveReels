a1b4775dfe9ef2c0b1c4f79970f17a3087c7336b
Himanshu Rathi <thehimanshurathi@gmail.com>
2024-11-06 23:11:43 +0530

    edit posts limit

---
+++ build/precache-manifest.72cd047ee0d660af7fe250d9c950631e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15429fbe6e1351be7eceb58478390da1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b56cc092a13448732ed3",
+    "url": "/static/css/main.4da26f39.chunk.css"
+  },
+  {
+    "revision": "5a8153dbcbbf24dd78ee",
+    "url": "/static/js/2.6ed282b4.chunk.js"
+  },
+  {
+    "revision": "2e7c71801ccd253048297129fe0fa2de",
+    "url": "/static/js/2.6ed282b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b56cc092a13448732ed3",
+    "url": "/static/js/main.ffd8b121.chunk.js"
+  },
+  {
+    "revision": "36f8b411c10ebbe9e99b",
+    "url": "/static/js/runtime-main.46ba0ce8.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const client = new dhive.Client("https://api.hive.blog");
 const getPosts = async () => {
   const query = {
     tag: "hive",
-    limit: 5,
+    limit: 20,
   };
 
   try {
